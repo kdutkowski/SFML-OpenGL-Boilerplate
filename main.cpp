426e43cf627d36340b4b7c2cb70806b8e35d1ee1
Chris Brenton <chrisb@ifixit.com>
2011-12-25 21:34:56 -0800

    Added multiple types of input - triggered (on key press or release) and controls (while key is held). Framerate can now be toggled with F1. Quit with Q. WASD and left mouse button produce messages.

--- main.cpp
+++ main.cpp
@@ -19,29 +19,35 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include <SFML/Window/Input.hpp>
 
 #define FPS_MAX 60
 
-#define mIsKey(x,y) (x.Key.Code == Key::y)
+//#define mIsKey(x,y) (x.Key.Code == Key::y)
+#define mIsKey(x,y) (x.IsKeyDown(Key::y))
 
 using namespace std;
 using namespace sf;
 
 bool running = true;
+bool showFramerate = true;
 int width, height, colorDepth;
 string windowTitle;
-Event event;
 Font hudFont;
 String text;
 float tick, fps;
-string fpsText;
 stringstream fpsStream;
+RenderWindow *app;
 
-void initialize(RenderWindow &app);
+void initialize();
 void setupGL(int width, int height);
+void controlKeys();
+void triggerKeyDown(Event e);
+void triggerKeyUp(Event e);
 void processEvent(Event e);
+void updateFramerate();
 
-void initialize(RenderWindow &app)
+void initialize()
 {
    // Setup OpenGL.
    setupGL(width, height);
@@ -56,7 +62,7 @@ void initialize(RenderWindow &app)
    text.SetScale(0.5f, 0.5f);
    text.Move(width - 150.f, height - 30.f);
 
-   app.SetFramerateLimit(FPS_MAX);
+   app->SetFramerateLimit(FPS_MAX);
 }
 
 void setupGL(int width, int height)
@@ -72,6 +78,50 @@ void setupGL(int width, int height)
    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 }
 
+void controlKeys()
+{
+   const Input& input = app->GetInput();
+   // Keys that are used as controls go here (i.e. activate while held).
+   if (mIsKey(input,W))
+   {
+      cout << "W" << endl;
+   }
+   if (mIsKey(input,A))
+   {
+      cout << "A" << endl;
+   }
+   if (mIsKey(input,S))
+   {
+      cout << "S" << endl;
+   }
+   if (mIsKey(input,D))
+   {
+      cout << "D" << endl;
+   }
+   if (input.IsMouseButtonDown(Mouse::Left))
+   {
+      cout << "LEFT MOUSE" << endl;
+   }
+}
+
+void triggerKeyDown(Event e)
+{
+   // Keys that trigger events go here (i.e. activate once on keypress).
+   if (e.Key.Code == Key::F1)
+   {
+      showFramerate = !showFramerate;
+   }
+}
+
+void triggerKeyUp(Event e)
+{
+   // Same as triggerKeyDown, but for keys that trigger on release.
+   if (e.Key.Code == Key::Q)
+   {
+      running = false;
+   }
+}
+
 void processEvent(Event e)
 {
    // Window closed
@@ -81,13 +131,35 @@ void processEvent(Event e)
    }
 
    // Key pressed
+   /*
+      if (e.Type == Event::KeyPressed ||
+      e.Type == Event::KeyReleased ||
+      e.Type == Event::MouseButtonPressed ||
+      e.Type == Event::MouseButtonReleased ||
+      e.Type == Event::JoyButtonPressed ||
+      e.Type == Event::JoyButtonReleased)
+   */
+
    if (e.Type == Event::KeyPressed)
    {
-      if (mIsKey(e, Q))
-      {
-         running = false;
-      }
+      triggerKeyDown(e);
    }
+   else if (e.Type == Event::KeyReleased)
+   {
+      triggerKeyUp(e);
+   }
+}
+
+void updateFramerate()
+{
+   // Get framerate.
+   tick = app->GetFrameTime();
+   fps = 1.f / tick;
+   fpsStream.str(string());
+   fpsStream << "FPS: " << fixed << setprecision(2) << fps;
+   text.SetText(fpsStream.str());
+   // Display the framerate.
+   app->Draw(text);
 }
 
 int main(int argc, char **argv)
@@ -97,33 +169,37 @@ int main(int argc, char **argv)
    colorDepth = 32;
    windowTitle = "I LIKE TURTLES";
    tick = 0.f;
+   Event event;
+
    // Set up window.
-   RenderWindow app(VideoMode(width, height, colorDepth), windowTitle);
+   app = new RenderWindow(VideoMode(width, height, colorDepth), windowTitle);
 
    // Initialize OpenGL.
-   initialize(app);
+   initialize();
 
    // Main loop.
    while (running)
    {
+      // Clear the window.
+      app->Clear();
+
       // Process events.
-      while (app.GetEvent(event))
+      controlKeys();
+      while (app->GetEvent(event))
       {
          processEvent(event);
       }
-      tick = app.GetFrameTime();
-      fps = 1.f / tick;
-      fpsStream.str(string());
-      fpsStream << "FPS: " << fixed << setprecision(2) << fps;
-      text.SetText(fpsStream.str());
-      app.Clear();
-      app.Draw(text);
+      if (showFramerate)
+      {
+         updateFramerate();
+      }
       // Update the main window.
-      app.Display();
+      app->Display();
    }
-   
+
    // Clean up.
-   app.Close();
+   app->Close();
+   delete app;
 
    // Exit.
    return EXIT_SUCCESS;
